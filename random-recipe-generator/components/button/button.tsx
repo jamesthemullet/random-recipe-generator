09169e0200995b28ff51589c71405be47e74d4cd
canderson-13 <98andersonc@gmail.com>
2022-09-22 13:30:02 +0100

    add styling

--- random-recipe-generator/components/button/button.tsx
+++ random-recipe-generator/components/button/button.tsx
@@ -2,18 +2,19 @@ import styled from "@emotion/styled";
 
 const StyledButton = styled.button`
   padding: 10px 20px;
-  background: white;
+  background: #81b29a;
   outline: none;
   border: 2px solid black;
   font-size: 18px;
   cursor: pointer;
+  font-family: 'Raleway';
 
   &:hover {
-    background: lightblue;
+    background: #e07a5f;
   }
 
   &:active {
-    background: blueviolet;
+    background: #e07a5f;
     color: white;
   }
 `;
